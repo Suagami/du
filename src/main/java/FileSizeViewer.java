885ab961611ffce471c89165e06491be5af3296d
Suagami <suagami@gmail.com>
2019-04-21 21:37:17 +0300

    Initial commit

---
+++ src/main/java/FileSizeViewer.java
@@ -0,0 +1,107 @@
+import javafx.util.Pair;
+import java.io.*;
+import java.util.*;
+
+import static java.lang.Math.round;
+
+public class FileSizeViewer {
+    private final List<String> fileNames;
+    Long sumSize = 0L;
+    List<Pair<String, Long>> table = new ArrayList<Pair<String, Long>>();
+
+
+
+    FileSizeViewer(List<String> fileNames) {
+        this.fileNames = fileNames;
+    }
+
+    public int size() {
+            for (String name: this.fileNames) {
+                File tempFile = new File(name);
+                long tempSize;
+                if (!tempFile.exists()) {
+                    System.err.println("This file or directory does not exist");
+                    return 1;
+                }
+                if (tempFile.isDirectory()) {
+                    tempSize = folderSize(tempFile);
+                } else tempSize = tempFile.length();
+                Pair<String, Long> newPair = new Pair<String, Long>(name, tempSize);
+                this.table.add(newPair);
+            }
+            return 0;
+    }
+
+    private long folderSize(File directory) {
+        long length = 0;
+        try {
+            for (File file : directory.listFiles()) {
+                if (file.isFile())
+                    length += file.length();
+                else
+                    length += folderSize(file);
+            }
+        }
+        catch (NullPointerException e) {}
+        return length;
+    }
+
+
+    void getSum(){
+        for(Pair size: this.table){
+            Long tempSize = (Long)size.getValue();
+            this.sumSize += tempSize;
+        }
+    }
+
+
+    private String humanisation(Long size, int base) {
+        String t = "B";
+        double rSize = (double) size;
+        if (rSize > base) {
+            rSize /= base;
+            t = "KB";
+            if (rSize > base) {
+                rSize = round(rSize);
+                rSize /= base;
+                t = "MB";
+                if (rSize > base) {
+                    rSize = round(rSize);
+                    rSize /= base;
+                    t = "GB";
+                }
+            }
+        }
+        String strSize = String.format("%.2f", rSize);
+        return strSize + t;
+    }
+
+
+    void output(int base, boolean isSum, boolean isHum) {
+        if (isSum) {
+            if (isHum) {
+                System.out.println("Total size: " + humanisation(this.sumSize, base));
+            } else {
+                System.out.println("Total size: " + this.sumSize + "B");
+            }
+        }
+        else {
+            if (isHum) {
+                for (Pair<String, Long> pair : this.table) {
+                    System.out.println(pair.getKey() + ": " + humanisation(pair.getValue(), base));
+                }
+            } else {
+                for (Pair<String, Long> pair : this.table) {
+                    System.out.println(pair.getKey() + ": " + pair.getValue() + "B");
+                }
+            }
+        }
+
+    }
+  //  public long view(boolean h, boolean c, boolean si) {
+  //      if (c) {
+   //         List<Long> tempSizes = FileSizeViewer.size();
+  //      }
+//
+
+}
---
+++ src/main/java/FileSizeViewerLauncher.java
@@ -0,0 +1,54 @@
+import org.kohsuke.args4j.Argument;
+import org.kohsuke.args4j.CmdLineException;
+import org.kohsuke.args4j.CmdLineParser;
+import org.kohsuke.args4j.Option;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FileSizeViewerLauncher {
+
+    @Option(name = "-h", metaVar = "ReadableVer", usage = "Making file's size readable")
+    private boolean hSize;
+
+    @Option(name = "-c", metaVar = "SummaryS", usage = "Summary size counting")
+    private boolean cSize;
+
+    @Option(name = "--si", metaVar = "DecimalBase", usage = "Changing base from 1024 to 1000")
+    private boolean siSize;
+
+    @Argument
+    private List<String> arguments = new ArrayList<String>();
+
+    public static void main(String[] args) throws IOException {
+        new FileSizeViewerLauncher().doMain(args);
+    }
+    private void doMain(String[] args) throws IOException {
+        CmdLineParser parser = new CmdLineParser(this);
+
+        try {
+            parser.parseArgument(args);
+            if (arguments.isEmpty()) throw new CmdLineException(parser, new Exception());
+        }
+        catch (CmdLineException e) {
+            System.err.println(e.getMessage());
+            System.err.println("java -jar du.jar [-h] [-c] [--si] file1 file2 file3 …");
+            parser.printUsage(System.err);
+            return;
+        }
+
+        FileSizeViewer FSV = new FileSizeViewer(arguments);
+        FSV.size();
+        if (cSize) {
+            FSV.getSum();
+        }
+        if (siSize) {
+            FSV.output(1000, cSize, hSize);
+        }
+        else FSV.output(1024, cSize, hSize);
+
+    }
+
+
+}
